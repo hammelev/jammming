887d106c9959c89912e98132550cfe03ef7bf622
Hammelev <hammelev@outlook.com>
2025-06-15 13:10:26 +0700

    - Removed TODO as the more information has been added to tracks - Updated to handle cases where no image is provided by the Spotify API - Updated css

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,12 +1,10 @@
 // Styles
 import styles from './track.module.css';
 
-// TODO: Add more information about tracks
-
 export default function Track({track}){
     return (
         <div className={styles['track-container']}>
-            <img src={track.album.images[2].url} alt={track.name} />
+            {track.album?.images[2]?.url && <img src={track.album?.images[2]?.url} alt={`${track.name} Album Cover`} />}
             <div className={styles['track-info']}>
                 <p className={styles['track-title']}>{track.name}</p>
                 <p className={styles['track-artist']}>{track.artists.reduce((acc, artist) => acc ? acc + ', ' + artist.name : artist.name, '')}</p>
